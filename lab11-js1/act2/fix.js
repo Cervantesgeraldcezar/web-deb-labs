fefc2fabfc13feadc74a26cb86d3665802a63967
Gerald Cervantes <cervantesgeraldcezar@gmail.com>
2025-10-21 00:25:17 +0800

    lab11_ac2

---
+++ lab11-js1/act2/fix.js
@@ -0,0 +1,6 @@
+// Display the corrected HTML content inside the div
+document.getElementById("box").innerHTML = `
+  <div style="text-align: center; padding: 20px; margin-bottom: 20px; background-color: red; color: white;">
+    <p>This box has multiple utility classes fixed!</p>
+  </div>
+`;
